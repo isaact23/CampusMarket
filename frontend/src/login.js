23f8d851f03d571ffdddeb03f9fc997550dba62b
Isaac Thompson <shufflesninja@gmail.com>
2024-10-20 14:20:34 -0400

    Add proxy for frontend dev, and add register function

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,17 +1,35 @@
 import axios from 'axios';
 
+export const register = async (username, email, password) => {
+    try {
+        const response = await axios.post('/api/register', {
+            username: username,
+            email: email,
+            password: password
+        })
+        console.log("Account created successfully")
+        console.log(response)
+        return response
+    } catch (error) {
+        console.error("Account creation failed:", error)
+        return null
+    }
+}
+
 export const login = async (username, password) => {
     try {
-        const response = await axios.post('https://dev-wwzvhvto6t4diopr.us.auth0.com/oauth/token', {
+        const response = await axios.post('/login/oauth/token', {
             grant_type: 'password',
             username: username,
             password: password,
             client_id: '7yFnbiDquoRvw8Xwdxncdo2YBpSEHpqq',
-            audience: 'https://dev-wwzvhvto6t4diopr.us.auth0.com/api/v2/',
+            client_secret: 'VVHVDJfKWsX2u9rcJwDEyL5wvSJxJ3yTmX3aDkm01fFZowNbsiIaot5SPxwAg1Y9'
         });
+        console.log("Logged in successfully")
+        console.log(response.data)
         return response.data;
     } catch (error) {
-        console.error('Error:', error);
+        console.error("Log in failed:", error);
         return null;
     }
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/login': {
+        target: 'https://dev-wwzvhvto6t4diopr.us.auth0.com',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
