5c737471ee188c6b00b02ca8dc31db04b79034c9
Isaac Thompson <shufflesninja@gmail.com>
2024-11-12 13:19:34 -0500

    Implement add_user

--- backend/database/database.py
+++ backend/database/database.py
@@ -7,7 +7,11 @@ from market_types import *
 from typing import List
 
 def add_product(product: Product) -> int:
+
+    # INSERT INTO dbo.Users (Username) VALUES ('shuffles');
+
     with get_conn() as conn:
+        cursor = conn.cursor()
         pass # Return Product ID
 
 def lookup_product(id: int) -> Product:
@@ -23,7 +27,20 @@ def search_products(query: Query) -> List[Product]:
     pass # Return array of products
 
 def add_user(user: User) -> int:
-    pass # Return User ID
+    print("Adding user " + user.username)
+
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM dbo.Users WHERE Username = ?", user.username)
+        res = cursor.fetchone()
+        if res is None:
+            cursor.execute("INSERT INTO dbo.Users (Username) OUTPUT INSERTED.ID VALUES (?)", user.username)
+            new_id = cursor.fetchone()[0]
+            print("Successfully added user " + user.username)
+            return new_id
+        else:
+            print("User " + user.username + " already exists. Returning their ID")
+            return res.ID
+
 
 def lookup_user(id: int) -> User:
     pass # Return user
@@ -54,4 +71,8 @@ def get_conn():
     conn = pyodbc.connect(AZURE_SQL_CONNECTIONSTRING, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
     return conn
 
-get_conn()
+print("Authenticating into database")
+conn = get_conn()
+print("Authenticated into database")
+
+print(add_user(User(0, "Randy")))
--- backend/database/market_types.py
+++ backend/database/market_types.py
@@ -1,25 +1,29 @@
 class Message:
-    def __init__(this, to_id, from_id, title, content):
+    def __init__(this, id, to_id, from_id, title, content):
+        this.id = id
         this.to_id = to_id
         this.from_id = from_id
         this.title = title
         this.content = content
 
 class Transaction:
-    def __init__(this, product_id, buying_user_id, selling_user_id):
+    def __init__(this, id, product_id, buying_user_id, selling_user_id):
+        this.id = id
         this.product_id = product_id
         this.buying_user_id = buying_user_id
         this.selling_user_id = selling_user_id
 
 class Product:
-    def __init__(this, name, description, price, owner_id):
+    def __init__(this, id, name, description, price, owner_id):
+        this.id = id
         this.name = name
         this.description = description
         this.price = price
         this.owner_id = owner_id
 
 class User:
-    def __init__(this, username):
+    def __init__(this, id, username):
+        this.id = id
         this.username = username
 
 class Query:
