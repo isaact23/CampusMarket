9f9a0e69e174a8f2525ace13382cc7430c40abff
Isaac Thompson <shufflesninja@gmail.com>
2024-11-14 23:47:02 -0500

    Remove Django

--- backend/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('test/', views.test_connection, name='test'),  # Changed from 'endpoint/' to 'test/' to match React code
-]
