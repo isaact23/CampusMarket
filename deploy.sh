c2d31dda97c2750d02ae0ba1766d83187f97489a
Isaac Thompson <shufflesninja@gmail.com>
2024-11-30 21:40:50 -0500

    Move Django run command inside backend Dockerfile

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#sudo az login --use-device-code
+#sudo az acr login --name campusmarket
 sudo docker-compose build
-sudo docker push campusmarket3.azurecr.io/campusmarket-frontend:latest
-sudo docker push campusmarket3.azurecr.io/campusmarket-backend:latest
+sudo docker push campusmarket.azurecr.io/campusmarket-frontend:latest
+sudo docker push campusmarket.azurecr.io/campusmarket-backend:latest
