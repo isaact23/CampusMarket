15471c185032238bb85ca726272ead8314a9ab9d
Isaac Thompson <shufflesninja@gmail.com>
2024-12-02 19:35:32 -0500

    Use navigate() for seamless window transition

--- frontend/src/components/Home/Toolbar/Toolbar.jsx
+++ frontend/src/components/Home/Toolbar/Toolbar.jsx
@@ -1,14 +1,24 @@
 import SearchBox from "./SearchBox/SearchBox.jsx"
 import {TokenContext} from "../../../contexts/TokenContext.jsx"
 import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import './Toolbar.css'
 
 const Toolbar = () => {
     const {token, setToken} = useContext(TokenContext)
     
+    const navigate = useNavigate()
+    
     const logout = () => {
         setToken(null)
-        window.location = '/login'
+        navigate('/login')
+    }
+    const openListings = () => {
+        navigate('/home/listings')
+    }
+    const openHome = () => {
+        navigate('/home')
     }
 
     return (
@@ -17,8 +27,8 @@ const Toolbar = () => {
                 <h2 className="w3-bar-item w3-left">CampusMarket</h2>
                 <p className="w3-bar-item w3-left w3-white">Token {token}</p>
                 <a className="w3-bar-item w3-button w3-right" onClick={logout}><h2>Log Out</h2></a>
-                <a href="/home/listings" className="w3-bar-item w3-button w3-right"><h2>Listings</h2></a>
-                <a href="/home" className="w3-bar-item w3-button w3-right"><h2>Home</h2></a>
+                <a className="w3-bar-item w3-button w3-right" onClick={openListings}><h2>Listings</h2></a>
+                <a className="w3-bar-item w3-button w3-right" onClick={openHome}><h2>Home</h2></a>
                 <div className="w3-bar-item w3-right w3-margin">
                     <SearchBox />
                 </div>
